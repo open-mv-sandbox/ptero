fc1602d75ed23247977b425ac7903a7a5ecb8956
Layl Bongers <3094382+LaylBongers@users.noreply.github.com>
2023-03-04 21:55:36 +0100

    Add some doc tests to family

--- crates/family/src/lib.rs
+++ crates/family/src/lib.rs
@@ -23,7 +23,60 @@ impl<T: 'static> Family for StaticFamily<T> {
     type Member<'a> = T;
 }
 
-/// Reference to an Option that can contain any `Family::Member`, checked at runtime.
+/// Mutable reference to an `Option` that can contain any `Family::Member`, checked at runtime.
+///
+/// ```
+/// # use family::*;
+/// # enum StrFamily {}
+/// # impl Family for StrFamily { type Member<'a> = &'a str; }
+/// #
+/// let value = String::from("some value");
+/// let mut option = Some(value.as_str());
+/// let option_mut = AnyOptionMut::new::<StrFamily>(&mut option);
+///
+/// // Dynamically downcast back to the option
+/// let result = option_mut.downcast::<StrFamily>();
+/// assert!(result.is_some());
+/// let taken = result.unwrap().take();
+/// assert!(taken.is_some());
+///
+/// // Afterwards, we can take the option back again
+/// assert!(option.is_none());
+/// ```
+///
+/// ```compile_fail
+/// # use family::*;
+/// # enum StrFamily {}
+/// # impl Family for StrFamily { type Member<'a> = &'a str; }
+/// #
+/// let value = String::from("some value");
+/// let mut option = Some(value.as_str());
+/// let option_mut = AnyOptionMut::new::<StrFamily>(&mut option);
+///
+/// // Option is no longer valid
+/// drop(option);
+///
+/// // So this fails to compile
+/// option_mut.downcast::<StrFamily>();
+/// ```
+///
+/// ```compile_fail
+/// # use family::*;
+/// # enum StrFamily {}
+/// # impl Family for StrFamily { type Member<'a> = &'a str; }
+/// #
+/// let value = String::from("some value");
+/// let mut option = Some(value.as_str());
+/// let option_mut = AnyOptionMut::new::<StrFamily>(&mut option);
+///
+/// let result = option_mut.downcast::<StrFamily>();
+///
+/// // Option is no longer valid
+/// drop(option);
+///
+/// // So this fails to compile
+/// assert!(result.is_some());
+/// ```
 pub struct AnyOptionMut<'a> {
     family_id: TypeId,
     slot_ptr: NonNull<u8>,
@@ -31,6 +84,7 @@ pub struct AnyOptionMut<'a> {
 }
 
 impl<'a> AnyOptionMut<'a> {
+    /// Create a new mutable reference to an option with a family member.
     pub fn new<'b, F>(slot: &'a mut Option<F::Member<'b>>) -> Self
     where
         'b: 'a,
