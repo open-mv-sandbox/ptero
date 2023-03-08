6b730b695c74d71d5107a81ac89120338bc92bdc
Layl Bongers <3094382+LaylBongers@users.noreply.github.com>
2023-03-08 19:42:59 +0100

    Allow borrowed input on mapping

--- crates/stewart-utils/src/map.rs
+++ crates/stewart-utils/src/map.rs
@@ -2,7 +2,8 @@ use std::marker::PhantomData;
 use std::sync::atomic::AtomicPtr;
 
 use anyhow::Error;
-use stewart::{ActorT, AddrT, AfterProcess, AfterReduce, Id, System};
+use family::{utils::FamilyT, Family};
+use stewart::{Actor, Addr, AddrT, AfterProcess, AfterReduce, Id, System};
 use tracing::instrument;
 
 /// Start actor that maps a value into another one.
@@ -12,10 +13,10 @@ pub fn start_map<F, A, B>(
     parent: Id,
     function: F,
     target: AddrT<B>,
-) -> Result<AddrT<A>, Error>
+) -> Result<Addr<A>, Error>
 where
-    F: FnMut(A) -> B + 'static,
-    A: 'static,
+    F: FnMut(A::Member<'_>) -> B + 'static,
+    A: Family,
     B: 'static,
 {
     let info = system.create_actor(parent)?;
@@ -29,21 +30,40 @@ where
     Ok(info.addr())
 }
 
+pub fn start_map_t<F, A, B>(
+    system: &mut System,
+    parent: Id,
+    mut function: F,
+    target: AddrT<B>,
+) -> Result<AddrT<A>, Error>
+where
+    F: FnMut(A) -> B + 'static,
+    A: 'static,
+    B: 'static,
+{
+    let function = move |a: <FamilyT<A> as Family>::Member<'_>| function(a.0);
+    start_map::<_, FamilyT<A>, _>(system, parent, function, target)
+}
+
 struct MapActor<F, A, B> {
     function: F,
     target: AddrT<B>,
     _a: PhantomData<AtomicPtr<A>>,
 }
 
-impl<F, A, B> ActorT for MapActor<F, A, B>
+impl<F, A, B> Actor for MapActor<F, A, B>
 where
-    F: FnMut(A) -> B + 'static,
-    A: 'static,
+    F: FnMut(A::Member<'_>) -> B + 'static,
+    A: Family,
     B: 'static,
 {
-    type Message = A;
+    type Family = A;
 
-    fn reduce(&mut self, system: &mut System, message: A) -> Result<AfterReduce, Error> {
+    fn reduce(
+        &mut self,
+        system: &mut System,
+        message: A::Member<'_>,
+    ) -> Result<AfterReduce, Error> {
         // Immediately re-route the message
         let message = (self.function)(message);
         system.handle(self.target, message);
