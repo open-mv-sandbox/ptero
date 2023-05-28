07ab1cb00b9ae11451dc390a2897093bb348cc8a
Layl Bongers <3094382+LaylBongers@users.noreply.github.com>
2023-05-28 20:41:23 +0200

    Update WGPU

--- crates/dacti-viewer-js/src/lib.rs
+++ crates/dacti-viewer-js/src/lib.rs
@@ -1,10 +1,15 @@
 mod surface;
 mod viewer;
 
+use tracing::Level;
+use tracing_wasm::WASMLayerConfigBuilder;
 pub use viewer::Viewer;
 
 /// Initialize global hooks that may not yet be initialized.
 fn init_hooks() {
     console_error_panic_hook::set_once();
-    tracing_wasm::set_as_global_default();
+    let config = WASMLayerConfigBuilder::new()
+        .set_max_level(Level::INFO)
+        .build();
+    tracing_wasm::set_as_global_default_with_config(config);
 }
